516465327e73e9544e24893a8264993a90c85aeb
Karthik-beta <karthikpar@protonmail.com>
2023-12-11 10:32:31 +0530

    1.0.11.12.23.1

--- src/app/components/microfunctions/shift/shift.component.ts
+++ src/app/components/microfunctions/shift/shift.component.ts
@@ -7,18 +7,35 @@ import { Component, OnInit, OnDestroy  } from '@angular/core';
 })
 export class ShiftComponent {
 
+
+    // 3 Shift Per Day
+    // getShiftText(): string {
+    //     const currentHour = new Date().getHours();
+    //     let shiftText = '';
+
+    //     if (currentHour >= 6 && currentHour < 14) {
+    //       shiftText = 'Shift FS : 06:00 to 14:00';
+    //     } else if (currentHour >= 14 && currentHour < 22) {
+    //       shiftText = 'Shift SS : 14:00 to 22:00';
+    //     } else {
+    //       shiftText = 'Shift NS : 22:00 to 06:00';
+    //     }
+
+    //     return shiftText;
+    // }
+
+
     getShiftText(): string {
         const currentHour = new Date().getHours();
         let shiftText = '';
 
-        if (currentHour >= 6 && currentHour < 14) {
-          shiftText = 'Shift FS : 06:00 to 14:00';
-        } else if (currentHour >= 14 && currentHour < 22) {
-          shiftText = 'Shift SS : 14:00 to 22:00';
-        } else {
-          shiftText = 'Shift NS : 22:00 to 06:00';
+        if (currentHour >= 8 && currentHour < 20) {
+          shiftText = 'Shift FS : 08:00 to 20:00';
+        }
+        else {
+          shiftText = 'Shift NS : 20:00 to 08:00';
         }
 
         return shiftText;
-      }
+    }
 }
